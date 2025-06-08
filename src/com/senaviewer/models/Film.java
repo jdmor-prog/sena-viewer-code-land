c68033547ab7251aeea70a36f2ea3121911f93d4
Javier Moreno <jdmoreno26@outlook.com>
2025-06-08 15:00:34 -0500

    feat: Create abstract class Film

---
+++ src/com/senaviewer/models/Film.java
@@ -0,0 +1,73 @@
+public abstract class Film {
+    private int id;
+    private String title;
+    private String genre;
+    private String creator;
+    private String duration;
+    private short year;
+    private boolean viewed;
+
+    public Film(String title, String genre, String creator, String duration) {
+        this.title = title;
+        this.genre = genre;
+        this.creator = creator;
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public boolean isViewed() {
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+}
